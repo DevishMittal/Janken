87d2678aa855007a2d2e79b8a83314185b1ba528
DevishMittal <devishmittal1@gmail.com>
2023-09-29 22:31:15 +0530

    One round function

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-//randomly returns rock paper from computer
-    // const rock = "Rock";
-    // const paper = "Paper";
-    // const scissor = "Scissor";
-function getComputerChoice(){
-
-    const arrForChoice = ["rock","paper"," scissor"];
-    const randomIndex = Math.floor(Math.random()*arrForChoice.length);
+function getComputerChoice() {
+    const arrForChoice = ["rock", "paper", "scissor"];
+    const randomIndex = Math.floor(Math.random() * arrForChoice.length);
     const randomChoice = arrForChoice[randomIndex];
     return randomChoice;
-}
-console.log(getComputerChoice());
+  }
+  //console.log(getComputerChoice());
+  
+  function oneRound() {
+    const computerSelection = getComputerChoice();
+    console.log(computerSelection);
+    const playerSelection = prompt("Enter rock, paper or scissor").toLowerCase();
+    //const computerSelection = getComputerChoice();
+    if (
+      (playerSelection === "rock" && computerSelection === "paper") ||
+      (playerSelection === "paper" && computerSelection === "scissor") ||
+      (playerSelection === "scissor" && computerSelection === "rock")
+    ) {
+      return `You lose, ${computerSelection} beats ${playerSelection}`;
+    } else if (playerSelection.toLocaleLowerCase() === computerSelection.toLocaleLowerCase()) {
+      return "it's a tie";
+    } else {
+      return "You Won";
+    }
+  }
+  
+  console.log(oneRound());
